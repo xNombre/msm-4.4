8f553157b6b474a2cf4c3c3ae59d2432f4ea95e5
xNombre <kartapolska@gmail.com>
2020-04-13 17:38:32 +0200

    cpufreq: Rip out cpu boosting completely from old driver
    We will use it only as stune booster, any cpu kicks will be done via libperfmgr.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -11,7 +11,7 @@
  * GNU General Public License for more details.
  */
 
-#define pr_fmt(fmt) "cpu-boost: " fmt
+#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -23,174 +23,19 @@
 #include <linux/input.h>
 #include <linux/time.h>
 
-struct cpu_sync {
-	int cpu;
-	unsigned int input_boost_min;
-	unsigned int input_boost_freq;
-};
-
-static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 static struct workqueue_struct *cpu_boost_wq;
-
+static struct delayed_work dynamic_stune_boost_rem;
 static struct work_struct input_boost_work;
-static bool input_boost_enabled;
 
-static unsigned int input_boost_ms = 40;
-module_param(input_boost_ms, uint, 0644);
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
-module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
-static unsigned int dynamic_stune_boost_ms = 40;
-module_param(dynamic_stune_boost_ms, uint, 0644);
-static struct delayed_work dynamic_stune_boost_rem;
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
-static struct delayed_work input_boost_rem;
 static u64 last_input_time;
-#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
-static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
-{
-	int i, ntokens = 0;
-	unsigned int val, cpu;
-	const char *cp = buf;
-	bool enabled = false;
+static unsigned int dynamic_stune_boost_ms;
+module_param(dynamic_stune_boost_ms, uint, 0644);
+static int dynamic_stune_boost;
+module_param(dynamic_stune_boost, uint, 0644);
 
-	while ((cp = strpbrk(cp + 1, " :")))
-		ntokens++;
-
-	/* single number: apply to all CPUs */
-	if (!ntokens) {
-		if (sscanf(buf, "%u\n", &val) != 1)
-			return -EINVAL;
-		for_each_possible_cpu(i)
-			per_cpu(sync_info, i).input_boost_freq = val;
-		goto check_enable;
-	}
-
-	/* CPU:value pair */
-	if (!(ntokens % 2))
-		return -EINVAL;
-
-	cp = buf;
-	for (i = 0; i < ntokens; i += 2) {
-		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
-			return -EINVAL;
-		if (cpu > num_possible_cpus())
-			return -EINVAL;
-
-		per_cpu(sync_info, cpu).input_boost_freq = val;
-		cp = strchr(cp, ' ');
-		cp++;
-	}
-
-check_enable:
-	for_each_possible_cpu(i) {
-		if (per_cpu(sync_info, i).input_boost_freq) {
-			enabled = true;
-			break;
-		}
-	}
-	input_boost_enabled = enabled;
-
-	return 0;
-}
-
-static int get_input_boost_freq(char *buf, const struct kernel_param *kp)
-{
-	int cnt = 0, cpu;
-	struct cpu_sync *s;
-
-	for_each_possible_cpu(cpu) {
-		s = &per_cpu(sync_info, cpu);
-		cnt += snprintf(buf + cnt, PAGE_SIZE - cnt,
-				"%d:%u ", cpu, s->input_boost_freq);
-	}
-	cnt += snprintf(buf + cnt, PAGE_SIZE - cnt, "\n");
-	return cnt;
-}
-
-static const struct kernel_param_ops param_ops_input_boost_freq = {
-	.set = set_input_boost_freq,
-	.get = get_input_boost_freq,
-};
-module_param_cb(input_boost_freq, &param_ops_input_boost_freq, NULL, 0644);
-
-/*
- * The CPUFREQ_ADJUST notifier is used to override the current policy min to
- * make sure policy min >= boost_min. The cpufreq framework then does the job
- * of enforcing the new policy.
- *
- * The sync kthread needs to run on the CPU in question to avoid deadlocks in
- * the wake up code. Achieve this by binding the thread to the respective
- * CPU. But a CPU going offline unbinds threads from that CPU. So, set it up
- * again each time the CPU comes back up. We can use CPUFREQ_START to figure
- * out a CPU is coming online instead of registering for hotplug notifiers.
- */
-static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
-				void *data)
-{
-	struct cpufreq_policy *policy = data;
-	unsigned int cpu = policy->cpu;
-	struct cpu_sync *s = &per_cpu(sync_info, cpu);
-	unsigned int ib_min = s->input_boost_min;
-
-	switch (val) {
-	case CPUFREQ_ADJUST:
-		if (!ib_min)
-			break;
-
-		pr_debug("CPU%u policy min before boost: %u kHz\n",
-			 cpu, policy->min);
-		pr_debug("CPU%u boost min: %u kHz\n", cpu, ib_min);
-
-		cpufreq_verify_within_limits(policy, ib_min, UINT_MAX);
-
-		pr_debug("CPU%u policy min after boost: %u kHz\n",
-			 cpu, policy->min);
-		break;
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block boost_adjust_nb = {
-	.notifier_call = boost_adjust_notify,
-};
-
-static void update_policy_online(void)
-{
-	unsigned int i;
-
-	/* Re-evaluate policy to trigger adjust notifier for online CPUs */
-	get_online_cpus();
-	for_each_online_cpu(i) {
-		pr_debug("Updating policy for CPU%d\n", i);
-		cpufreq_update_policy(i);
-	}
-	put_online_cpus();
-}
-
-static void do_input_boost_rem(struct work_struct *work)
-{
-	unsigned int i;
-	struct cpu_sync *i_sync_info;
-
-	/* Reset the input_boost_min for all CPUs in the system */
-	pr_debug("Resetting input boost min for all CPUs\n");
-	for_each_possible_cpu(i) {
-		i_sync_info = &per_cpu(sync_info, i);
-		i_sync_info->input_boost_min = 0;
-	}
-
-	/* Update policies for all online CPUs */
-	update_policy_online();
-}
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 static void do_dynamic_stune_boost_rem(struct work_struct *work)
 {
 	/* Reset dynamic stune boost value to the default value */
@@ -199,34 +44,18 @@ static void do_dynamic_stune_boost_rem(struct work_struct *work)
 		stune_boost_active = false;
 	}
 }
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 static void do_input_boost(struct work_struct *work)
 {
-	unsigned int i, ret;
-	struct cpu_sync *i_sync_info;
+	unsigned int ret;
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	cancel_delayed_work_sync(&dynamic_stune_boost_rem);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-	cancel_delayed_work_sync(&input_boost_rem);
 
 	if (stune_boost_active) {
 		reset_stune_boost("top-app", boost_slot);
 		stune_boost_active = false;
 	}
 
-	/* Set the input_boost_min for all CPUs in the system */
-	pr_debug("Setting input boost min for all CPUs\n");
-	for_each_possible_cpu(i) {
-		i_sync_info = &per_cpu(sync_info, i);
-		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;
-	}
-
-	/* Update policies for all online CPUs */
-	update_policy_online();
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */
 	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
 	if (!ret)
@@ -234,10 +63,6 @@ static void do_input_boost(struct work_struct *work)
 
 	queue_delayed_work(cpu_boost_wq, &dynamic_stune_boost_rem,
 					msecs_to_jiffies(dynamic_stune_boost_ms));
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
-	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
-					msecs_to_jiffies(input_boost_ms));
 }
 
 static void cpuboost_input_event(struct input_handle *handle,
@@ -245,7 +70,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 {
 	u64 now;
 
-	if (!input_boost_enabled)
+	if (!dynamic_stune_boost)
 		return;
 
 	now = ktime_to_us(ktime_get());
@@ -291,10 +116,8 @@ err2:
 
 static void cpuboost_input_disconnect(struct input_handle *handle)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
 	reset_stune_boost("top-app", boost_slot);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	input_close_device(handle);
 	input_unregister_handle(handle);
@@ -337,24 +160,15 @@ static struct input_handler cpuboost_input_handler = {
 
 static int cpu_boost_init(void)
 {
-	int cpu, ret;
-	struct cpu_sync *s;
+	int ret;
 
 	cpu_boost_wq = alloc_workqueue("cpuboost_wq", WQ_HIGHPRI, 0);
 	if (!cpu_boost_wq)
 		return -EFAULT;
 
 	INIT_WORK(&input_boost_work, do_input_boost);
-	INIT_DELAYED_WORK(&input_boost_rem, do_input_boost_rem);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	INIT_DELAYED_WORK(&dynamic_stune_boost_rem, do_dynamic_stune_boost_rem);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
-	for_each_possible_cpu(cpu) {
-		s = &per_cpu(sync_info, cpu);
-		s->cpu = cpu;
-	}
-	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
 	ret = input_register_handler(&cpuboost_input_handler);
 
 	return ret;
